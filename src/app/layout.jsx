3f131dff6f645839494fe5b3caae224fcd88daf9
Fares zait <fares.plackfurt12@gmail.com>
2025-02-24 15:40:08 +0100

    another jsonchanges

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import StoreProvider from "@/lib/provider"
+import {StoreProvider} from "@/lib/Provider"
 import Adds from "@/components/Adds"
 
 
--- src/lib/Provider.jsx
+++ src/lib/Provider.jsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useRef } from "react"
 import store from "./store"
 import {Provider} from "react-redux"
